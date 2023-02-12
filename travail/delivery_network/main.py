e72b956e31ea5e04661c7f982c26d40e4f008be7
tttienthinh <tranthuongtienthinh@gmail.com>
2023-02-12 13:46:33 +0100

    graphvizé

--- travail/delivery_network/main.py
+++ travail/delivery_network/main.py
@@ -1,8 +1,19 @@
 from graph import Graph, graph_from_file
-
+import graphviz
 
 data_path = "input/"
 file_name = "network.01.in"
 
 g = graph_from_file(data_path + file_name)
 print(g)
+
+dot = graphviz.Digraph(comment='The Round Table')
+dot.node('A', 'King Arthur')  # doctest: +NO_EXE
+dot.node('B', 'Sir Bedevere the Wise')
+dot.node('L', 'Sir Lancelot the Brave')
+
+
+dot.edges(['AB', 'AL'])
+dot.edge('B', 'L', constraint='false')
+print(dot.source)
+dot.render('doctest-output/round-table.gv', view=True)
