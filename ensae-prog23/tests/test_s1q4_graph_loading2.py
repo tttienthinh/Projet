6a2cd7fba12e5a74f34b3ea8a6a12d0698fccbfa
tttienthinh <tranthuongtienthinh@gmail.com>
2023-02-23 17:48:34 +0100

    merge folders

--- ensae-prog23/tests/test_s1q4_graph_loading2.py
+++ ensae-prog23/tests/test_s1q4_graph_loading2.py
@@ -1,1 +1,23 @@
 # À compléter
+# This will work if ran from the root folder.
+import sys 
+sys.path.append("delivery_network/")
+
+import unittest 
+from graph import Graph, graph_from_file
+
+class Test_GraphLoading(unittest.TestCase):
+    def test_network4(self):
+        g = graph_from_file("input/network.04.in")
+        #### refaire les test là c'est ceux d'un autre test pour l'instant
+        self.assertEqual(g.nb_nodes, 10)
+        self.assertEqual(g.nb_edges, 4)
+        self.assertEqual(g.graph[1][0][2], 6) # This line test the importation of dist
+        self.assertEqual(g.graph[2][0][2], 3) # This line test the importation of dist
+        self.assertEqual(g.graph[3][1][2], 2) # This line test the importation of dist
+        self.assertEqual(g.graph[1][0][2], 6) # This line test the importation of dist
+        self.assertEqual(g.graph[2][1][2], 89) # This line test the importation of dist
+
+        
+if __name__ == '__main__':
+    unittest.main()
