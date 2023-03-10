5f6270e3180d8be9c377d35a2fbaa836a4f8baa1
amjn480 <85102198+amjn480@users.noreply.github.com>
2023-03-10 18:19:35 +0100

    la fonction de hauteur est désormais de meilleur complexité (j'avais fait un truc éclaté au sol)

--- ensae-prog23/tests/test_recursive_parc_connex.py
+++ ensae-prog23/tests/test_recursive_parc_connex.py
@@ -9,16 +9,7 @@ class Test_recursive_parc_connex(unittest.TestCase):
     def test_network0(self):
         graph = graph_from_file("input/network.00.in")
         height = graph.recursive_parc_connex(root=1)
-        self.assertTrue((1, 0) in height)
-        self.assertTrue((2, 1) in height)
-        self.assertTrue((6, 1) in height)
-        self.assertTrue((8, 1) in height)
-        self.assertTrue((3, 2) in height)
-        self.assertTrue((5, 2) in height)
-        self.assertTrue((9, 2) in height)
-        self.assertTrue((4, 3) in height)
-        self.assertTrue((7, 3) in height)
-        self.assertTrue((10, 4) in height)# add assertion here
+        self.assertEqual(height, {1: 0, 2: 1, 6: 1, 8: 1, 3: 2, 5: 2, 9: 2, 4: 3, 7: 3, 10: 4 })
 
 
 if __name__ == '__main__':
